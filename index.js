06ed635edd82b601863026ac2aab61c6c83b4a9c
xzj <1353753283@qq.com>
2025-03-05 10:33:24 +0800

    up

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.get('/api/hello', (req, res) => { res.json({ message: 'Hello World' }); });
-
-app.listen(443, () => { console.log('Server is running on port 3000'); });
